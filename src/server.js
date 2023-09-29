03b34b6a988ef378841d7ea13e7a9cae56797477
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-09-29 12:35:12 +0200

    websockets update

--- src/server.js
+++ src/server.js
@@ -8,10 +8,15 @@ const HOST = "0.0.0.0"; // This means listening on all network interfaces
 const app = express();
 
 const server = http.createServer(app); // Create an HTTP server
-const io = socketIo(server); // Bind socket.io to that server
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000", //This will allow any origin to connect. Adjust this if you want to restrict origins.
+    methods: ["GET", "POST"],
+  },
+}); // Bind socket.io to that server
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 
 io.on("connection", (socket) => {
   console.log("New client connected");
