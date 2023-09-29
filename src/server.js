6362754432b708bcdf6ed00a492ece56e920419c
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-09-29 12:23:53 +0200

    websockets

--- src/server.js
+++ src/server.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const PORT = 4000;
 const HOST = "0.0.0.0"; // This means listening on all network interfaces
 const app = express();
 
+const server = http.createServer(app); // Create an HTTP server
+const io = socketIo(server); // Bind socket.io to that server
+
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-app.use("/", require("./ledState/ledRoute.js"));
+io.on("connection", (socket) => {
+  console.log("New client connected");
 
-app.listen(PORT, HOST, () => {
+  socket.on("ledStateChange", (data) => {
+    console.log("LED state: ", data.ledState);
+    currentLedState = data.ledState; // Update our makeshift database
+    // Inform other clients (e.g., ESP32) about the change
+    io.emit("ledStateChange", { ledState: currentLedState });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// app.use("/", require("./ledState/ledRoute.js"));
+
+server.listen(PORT, HOST, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
