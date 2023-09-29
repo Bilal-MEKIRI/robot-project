5a4614d768161ba261d2f3d113ccc304f7dd6bfb
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-09-29 15:58:09 +0200

    potentiometer removed

--- src/potValue/potRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { postPotValue, getPotValue } = require("./potController.js");
-
-router.post("/postPotValue", postPotValue);
-router.get("/getPotValue", getPotValue);
-
-module.exports = router;
--- src/server.js
+++ src/server.js
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 app.use("/", require("./ledState/ledRoute.js"));
-app.use("/", require("./potValue/potRoute.js"));
 
 app.listen(PORT, HOST, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
